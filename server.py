499abafd401c81c1cab620129f188013edfe5477
Oleg <voleg239@gmail.com>
2026-04-29 10:34:07 -0400

    add Jira task creation

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import requests
 import os
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
+from atlassian import Jira
 
 load_dotenv()
 
@@ -11,6 +12,35 @@ CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
 AUTHORITY = "https://login.microsoftonline.com/common"
 # SCOPES = ["Mail.Read", "Mail.Send", "Chat.Read"]
 SCOPES = ["Mail.Read", "Mail.Send"]
+
+JIRA_URL = "https://oleg-dev-projects.atlassian.net"
+JIRA_EMAIL = "voleg239@gmail.com"
+JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
+
+jira = Jira (
+    url=JIRA_URL,
+    username=JIRA_EMAIL,
+    password=JIRA_API_TOKEN,
+    cloud=True
+)
+
+mcp = FastMCP("Oleg-MCP")
+
+
+@mcp.tool()
+def create_jira_task(summary: str, description: str):
+    """Creates a new task in the Jira backlog."""
+    try:
+        issue_dict = {
+            'project': {'key': 'KAN'}, 
+            'summary': summary,
+            'description': description,
+            'issuetype': {'name': 'Task'}, 
+        }
+        new_issue = jira.create_issue(fields=issue_dict)
+        return f"Success! Ticket created: {new_issue['key']}"
+    except Exception as e:
+        return f"Jira Technical Error: {str(e)}"
 
 def get_ms_token():
     base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -45,7 +75,6 @@ def get_ms_token():
 
     return None
 
-mcp = FastMCP("Oleg-MCP")
 
 # @mcp.tool()
 # def list_teams_chats(count: int = 10):
